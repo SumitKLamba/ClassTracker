da6c2e8799c9e1af1621da8955371731bcaae403
sumit <sumitskl1999@gmail.com>
2019-12-09 20:53:34 +0530

    half done

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -14,55 +14,67 @@ class Profile_Page extends StatelessWidget {
                   colors: [Color(0xFF1E272E), Color(0xFF250505)]),
             ),
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.width,
-            child: Stack(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(top: 70),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: MediaQuery.of(context).size.width-120,
-                      width: MediaQuery.of(context).size.width-120,
-                      decoration: BoxDecoration(
-                        color: Color(0x54FF3F34),
-                        borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+          Column(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width,
+                child: Stack(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: MediaQuery.of(context).size.width-120,
+                          width: MediaQuery.of(context).size.width-120,
+                          decoration: BoxDecoration(
+                            color: Color(0x54FF3F34),
+                            borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 70),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: MediaQuery.of(context).size.width-170,
-                      width: MediaQuery.of(context).size.width-170,
-                      decoration: BoxDecoration(
-                        color: Color(0x54FF3F34),
-                        borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: MediaQuery.of(context).size.width-170,
+                          width: MediaQuery.of(context).size.width-170,
+                          decoration: BoxDecoration(
+                            color: Color(0x54FF3F34),
+                            borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 70),
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: MediaQuery.of(context).size.width-220,
-                      width: MediaQuery.of(context).size.width-220,
-                      decoration: BoxDecoration(
-                        color: Color(0x54FF3F34),
-                        borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+                    Padding(
+                      padding: EdgeInsets.only(top: 50),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Container(
+                          height: MediaQuery.of(context).size.width-220,
+                          width: MediaQuery.of(context).size.width-220,
+                          decoration: BoxDecoration(
+                            color: Color(0x54FF3F34),
+                            borderRadius: BorderRadius.all(Radius.circular(MediaQuery.of(context).size.width-70),),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
+              ),
+              Text(
+                'Gurkaran Singh',
+                style: TextStyle(fontFamily: 'Raleway',
+                    fontSize: 40,
+                color: Color(0xFFC2C7D1)),
+              ),
+              SizedBox(height: 30,),
+
+            ],
           ),
         ],
       ),
